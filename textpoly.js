16274faef3ab11487b4baa90eb5d771b68a36422
denes-sebestyen <163525320+denes-sebestyen@users.noreply.github.com>
2024-06-29 14:11:51 +0200

    Lots of changes
    Yeah, that's on me, I should not make this big changes, but hey, it's the beginning of the project :)
    - add memoization to createTextBoxes in order to prevent repeated calculation
    - add brand new method to create text boxes, it should be better, but still needs testing

--- textpoly.js
+++ textpoly.js
@@ -1,11 +1,13 @@
 export class Textpoly {
   #polygons;
+  #memoizedBoxes = null;
   options = {
     lineHeight: 15,
     marginTop: 0,
     marginBottom: 0,
     marginLeft: 0,
-    marginRight: 0
+    marginRight: 0,
+    method: 1
   };
 
   #isPoint(point) {
@@ -39,59 +41,142 @@ export class Textpoly {
 
   emptyPolygons() {
     this.#polygons = [];
+    this.#memoizedBoxes = null;
   }
 
   addPolygon(polygon) {
     if (typeof polygon==='string') polygon = this.#processString(polygon);
     if (!this.#isPolygon(polygon)) throw new Error(`can not add polygon '${polygon}', because its format is not valid`);
     this.#polygons.push(polygon);
+    this.#memoizedBoxes = null;
   }
 
   createTextBoxes() {
+    if (this.#memoizedBoxes) { return this.#memoizedBoxes; }
     const textBoxes = [];
+    const lineHeight = this.options.lineHeight;
     this.#polygons.forEach(polygon => {
       const copyPoly = polygon.map((point, idx) => ({ ...point, idx }));
+      const minX = Math.min(...polygon.map(point => point.x));
+      const maxX = Math.max(...polygon.map(point => point.x));
       const minY = Math.min(...polygon.map(point => point.y));
       const maxY = Math.max(...polygon.map(point => point.y));
       const lines = copyPoly.map((A, idx) => ({ A, B: copyPoly[idx==copyPoly.length-1 ? 0 : idx+1]}))
         .map(line => line.A.x > line.B.x ? { A: line.B, B: line.A } : line);
       lines.sort((l1, l2) => l1.A.x == l2.A.x ? l1.B.x - l2.B.x : l1.A.x - l2.A.x);
       const from = minY+this.options.marginTop;
-      const till = maxY-this.options.marginBottom-this.options.lineHeight;
-      for (let y=from; y<=till; y+=this.options.lineHeight) {
-        const y2 = y+this.options.lineHeight;
-        let startX = null;
-        let topCrosses = [];
-        let bottomCrosses = [];
+      const till = maxY-this.options.marginBottom;
+      if (this.options.method === 1) {
+        const rows = new Array(Math.floor((till-from) / lineHeight))
+          .fill({})
+          .map((_, idx) => ({ top: from+idx*lineHeight, bottom: from+(idx+1)*lineHeight, segments: [], build: {} }));
         lines.forEach(({ A, B }) => {
-          const ABy = A.y - B.y;
-          if (ABy !== 0) {
-            const t1 = (y-B.y) / ABy;
-            if (t1 >= 0 && t1 <= 1) {
-              topCrosses.push((1-t1)*B.x + t1*(A.x));
-            }
-            const t2 = (y2-B.y) / ABy;
-            if (t2 >= 0 && t2 <= 1) {
-              bottomCrosses.push((1-t2)*B.x + t2*(A.x));
-            }
-          }
-          if (startX === null) {
-            if (topCrosses.length > 0 && bottomCrosses.length > 0) {
-              startX = Math.max(...topCrosses, ...bottomCrosses);
-              topCrosses = [];
-              bottomCrosses = [];
-            }
-          } else {
-            if (topCrosses.length > 0 && bottomCrosses.length > 0) {
-              textBoxes.push({ startX, endX: Math.min(...topCrosses, ...bottomCrosses), y, y2 });
-              topCrosses = [];
-              bottomCrosses = [];
-            }
-          }
+          const [ upper, lower ] = A.y < B.y ? [ A, B] : [ B, A ];
+          const LUy = lower.y - upper.y;
+          rows.filter(row => row.bottom > upper.y && row.top < lower.y)
+            .forEach((row, idx, rowsFiltered) => {
+              const build = row.build;
+              if (idx === 0 && rowsFiltered.length === 0) { // only row
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right ?? maxX, A.x, B.x);
+                } else {
+                  build.left = Math.max(build.left ?? minX, A.x, B.x);
+                }
+              } else if (upper.y > row.top) { // first row
+                const t = (row.bottom - upper.y) / LUy;
+                const bottomX = (1-t)*upper.x + t*(lower.x);
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right ?? maxX, upper.x, bottomX);
+                  build.bottomRight = true;
+                } else {
+                  build.left = Math.max(build.left ?? minX, upper.x, bottomX);
+                  build.bottomLeft = true;
+                }
+                build.bottomX = bottomX;
+              } else if (lower.y > row.bottom) { // middle rows
+                let topX;
+                if (idx > 0) {
+                  topX = rowsFiltered[idx-1].build.bottomX;
+                } else {
+                  const t = (row.top - upper.y) / LUy;
+                  topX = (1-t)*upper.x + t*(lower.x);
+                }
+                const t = (row.bottom - upper.y) / LUy;
+                const bottomX = (1-t)*upper.x + t*(lower.x);
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right ?? maxX, topX, bottomX);
+                  build.topRight = true;
+                  build.bottomRight = true;
+                } else {
+                  build.left = Math.max(build.left ?? minX, topX, bottomX);
+                  build.topLeft = true;
+                  build.bottomLeft = true;
+                }
+                build.bottomX = bottomX;
+              } else { // last row
+                let topX;
+                if (idx > 0) {
+                  topX = rowsFiltered[idx-1].build.bottomX;
+                } else {
+                  const t = (row.top - upper.y) / LUy;
+                  topX = (1-t)*upper.x + t*(lower.x);
+                }
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right ?? maxX, topX, lower.x);
+                  build.topRight = true;
+                } else {
+                  build.left = Math.max(build.left ?? minX, topX, lower.x);
+                  build.topLeft = true;
+                }
+              }
+              if (build.topLeft && build.topRight && build.bottomLeft && build.bottomRight) {
+                row.segments.push({ left: build.left, right: build.right });
+                build.topLeft = false;
+                build.topRight = false;
+                build.bottomLeft = false;
+                build.bottomRight = false;
+              }
+            });
         });
+        rows.forEach(row => row.segments.forEach(segment => {
+          textBoxes.push({ startX: segment.left, endX: segment.right, y: row.top, y2: row.bottom })
+        }));
+      } else {
+        for (let y=from; y<till; y+=lineHeight) {
+          const y2 = y+lineHeight;
+          let startX = null;
+          let topCrosses = [];
+          let bottomCrosses = [];
+          lines.forEach(({ A, B }) => {
+            const ABy = A.y - B.y;
+            if (ABy !== 0) {
+              const t1 = (y-B.y) / ABy;
+              if (t1 >= 0 && t1 <= 1) {
+                topCrosses.push((1-t1)*B.x + t1*(A.x));
+              }
+              const t2 = (y2-B.y) / ABy;
+              if (t2 >= 0 && t2 <= 1) {
+                bottomCrosses.push((1-t2)*B.x + t2*(A.x));
+              }
+            }
+            if (startX === null) {
+              if (topCrosses.length > 0 && bottomCrosses.length > 0) {
+                startX = Math.max(...topCrosses, ...bottomCrosses);
+                topCrosses = [];
+                bottomCrosses = [];
+              }
+            } else {
+              if (topCrosses.length > 0 && bottomCrosses.length > 0) {
+                textBoxes.push({ startX, endX: Math.min(...topCrosses, ...bottomCrosses), y, y2 });
+                topCrosses = [];
+                bottomCrosses = [];
+              }
+            }
+          });
+        }
       }
     });
-    return textBoxes;
+    return this.#memoizedBoxes = textBoxes;
   }
 
   createTextPath() {
--- textpoly.ts
+++ textpoly.ts
@@ -14,12 +14,14 @@ export interface TextBox {
 
 export class Textpoly {
   private polygons: Polygon[];
+  private memoizedBoxes: TextBox[] | null = null;
   options = {
     lineHeight: 15,
     marginTop: 0,
     marginBottom: 0,
     marginLeft: 0,
-    marginRight: 0
+    marginRight: 0,
+    method: 1
   };
 
   private isPoint(point): point is Point {
@@ -52,59 +54,153 @@ export class Textpoly {
 
   emptyPolygons() {
     this.polygons = [];
+    this.memoizedBoxes = null;
   }
 
   addPolygon(polygon: Polygon | string) {
     if (typeof polygon==='string') polygon = this.processString(polygon);
     if (!this.isPolygon(polygon)) throw new Error(`can not add polygon '${polygon}', because its format is not valid`);
     this.polygons.push(polygon);
+    this.memoizedBoxes = null;
   }
 
   createTextBoxes(): TextBox[] {
+    if (this.memoizedBoxes) { return this.memoizedBoxes; }
     const textBoxes: TextBox[] = [];
+    const lineHeight = this.options.lineHeight;
     this.polygons.forEach(polygon => {
       const copyPoly = polygon.map((point, idx) => ({ ...point, idx }));
+      const minX = Math.min(...polygon.map(point => point.x));
+      const maxX = Math.max(...polygon.map(point => point.x));
       const minY = Math.min(...polygon.map(point => point.y));
       const maxY = Math.max(...polygon.map(point => point.y));
       const lines = copyPoly.map((A, idx) => ({ A, B: copyPoly[idx==copyPoly.length-1 ? 0 : idx+1]}))
         .map(line => line.A.x > line.B.x ? { A: line.B, B: line.A } : line);
       lines.sort((l1, l2) => l1.A.x == l2.A.x ? l1.B.x - l2.B.x : l1.A.x - l2.A.x);
       const from = minY+this.options.marginTop;
-      const till = maxY-this.options.marginBottom-this.options.lineHeight;
-      for (let y=from; y<=till; y+=this.options.lineHeight) {
-        const y2 = y+this.options.lineHeight;
-        let startX: number | null = null;
-        let topCrosses: number[] = [];
-        let bottomCrosses: number[] = [];
+      const till = maxY-this.options.marginBottom-lineHeight;
+      interface Segment { left: number; right: number }
+      interface Build {
+        topLeft: boolean; topRight: boolean; bottomLeft: boolean; bottomRight: boolean;
+        left: number; right: number; bottomX?: number;
+      }
+      interface Row { top: number; bottom: number; segments: Segment[]; build: Build }
+      if (this.options.method === 1) {
+        const defaultBuild: Build = {
+          topLeft: false, topRight: false, bottomLeft: false, bottomRight: false,
+          left: minX, right: maxX, bottomX: 0
+        }
+        const rows: Row[] = new Array(Math.floor((till-from) / lineHeight))
+          .fill({})
+          .map((_, idx) => ({ top: from+idx*lineHeight, bottom: from+(idx+1)*lineHeight, segments: [], build: { ...defaultBuild } }));
         lines.forEach(({ A, B }) => {
-          const ABy = A.y - B.y;
-          if (ABy !== 0) {
-            const t1 = (y-B.y) / ABy;
-            if (t1 >= 0 && t1 <= 1) {
-              topCrosses.push((1-t1)*B.x + t1*(A.x));
-            }
-            const t2 = (y2-B.y) / ABy;
-            if (t2 >= 0 && t2 <= 1) {
-              bottomCrosses.push((1-t2)*B.x + t2*(A.x));
-            }
-          }
-          if (startX === null) {
-            if (topCrosses.length > 0 && bottomCrosses.length > 0) {
-              startX = Math.max(...topCrosses, ...bottomCrosses);
-              topCrosses = [];
-              bottomCrosses = [];
-            }
-          } else {
-            if (topCrosses.length > 0 && bottomCrosses.length > 0) {
-              textBoxes.push({ startX, endX: Math.min(...topCrosses, ...bottomCrosses), y, y2 });
-              topCrosses = [];
-              bottomCrosses = [];
-            }
-          }
+          const [ upper, lower ] = A.y < B.y ? [ A, B] : [ B, A ];
+          const LUy = lower.y - upper.y;
+          rows.filter(row => row.bottom > upper.y && row.top < lower.y)
+            .forEach((row, idx, rowsFiltered) => {
+              const build = row.build;
+              if (idx === 0 && rowsFiltered.length === 0) { // only row
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right, A.x, B.x);
+                } else {
+                  build.left = Math.max(build.left, A.x, B.x);
+                }
+              } else if (upper.y > row.top) { // first row
+                const t = (row.bottom - upper.y) / LUy;
+                const bottomX = (1-t)*upper.x + t*(lower.x);
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right, upper.x, bottomX);
+                  build.bottomRight = true;
+                } else {
+                  build.left = Math.max(build.left, upper.x, bottomX);
+                  build.bottomLeft = true;
+                }
+                build.bottomX = bottomX;
+              } else if (lower.y > row.bottom) { // middle rows
+                let topX;
+                if (idx > 0) {
+                  topX = rowsFiltered[idx-1].build.bottomX;
+                } else {
+                  const t = (row.top - upper.y) / LUy;
+                  topX = (1-t)*upper.x + t*(lower.x);
+                }
+                const t = (row.bottom - upper.y) / LUy;
+                const bottomX = (1-t)*upper.x + t*(lower.x);
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right, topX, bottomX);
+                  build.topRight = true;
+                  build.bottomRight = true;
+                } else {
+                  build.left = Math.max(build.left, topX, bottomX);
+                  build.topLeft = true;
+                  build.bottomLeft = true;
+                }
+                build.bottomX = bottomX;
+              } else { // last row
+                let topX;
+                if (idx > 0) {
+                  topX = rowsFiltered[idx-1].build.bottomX;
+                } else {
+                  const t = (row.top - upper.y) / LUy;
+                  topX = (1-t)*upper.x + t*(lower.x);
+                }
+                if (build.topLeft && build.bottomLeft) {
+                  build.right = Math.min(build.right, topX, lower.x);
+                  build.topRight = true;
+                } else {
+                  build.left = Math.max(build.left, topX, lower.x);
+                  build.topLeft = true;
+                }
+              }
+              if (build.topLeft && build.topRight && build.bottomLeft && build.bottomRight) {
+                row.segments.push({ left: build.left, right: build.right });
+                row.build = { ...defaultBuild };
+                build.topLeft = false;
+                build.topRight = false;
+                build.bottomLeft = false;
+                build.bottomRight = false;
+              }
+            });
         });
+        rows.forEach(row => row.segments.forEach(segment => {
+          textBoxes.push({ startX: segment.left, endX: segment.right, y: row.top, y2: row.bottom })
+        }));
+      } else {
+        for (let y=from; y<=till; y+=lineHeight) {
+          const y2 = y+lineHeight;
+          let startX: number | null = null;
+          let topCrosses: number[] = [];
+          let bottomCrosses: number[] = [];
+          lines.forEach(({ A, B }) => {
+            const ABy = A.y - B.y;
+            if (ABy !== 0) {
+              const t1 = (y-B.y) / ABy;
+              if (t1 >= 0 && t1 <= 1) {
+                topCrosses.push((1-t1)*B.x + t1*(A.x));
+              }
+              const t2 = (y2-B.y) / ABy;
+              if (t2 >= 0 && t2 <= 1) {
+                bottomCrosses.push((1-t2)*B.x + t2*(A.x));
+              }
+            }
+            if (startX === null) {
+              if (topCrosses.length > 0 && bottomCrosses.length > 0) {
+                startX = Math.max(...topCrosses, ...bottomCrosses);
+                topCrosses = [];
+                bottomCrosses = [];
+              }
+            } else {
+              if (topCrosses.length > 0 && bottomCrosses.length > 0) {
+                textBoxes.push({ startX, endX: Math.min(...topCrosses, ...bottomCrosses), y, y2 });
+                topCrosses = [];
+                bottomCrosses = [];
+              }
+            }
+          });
+        }
       }
     });
-    return textBoxes;
+    return this.memoizedBoxes = textBoxes;
   }
 
   createTextPath(): string {
